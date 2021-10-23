4c378f70f08dcf523bcc27b6b25b5fce2939a708
Aravin7 <aravinth.ati@gmail.com>
2021-10-24 00:31:03 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Demo from './demo';
+import Dashboard from './container/authUser/Dashboard';
 import { Test } from './Test';
 import './main.css';
 
 ReactDOM.render(
   <Router>
     <StyledEngineProvider injectFirst>
-      <Test />
+      <Dashboard />
     </StyledEngineProvider>
   </Router>,
   document.querySelector('#root')
